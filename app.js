5b056354ec467aa57947d23628e01d77ae51bf9e
Joao Victor <jsousa.1998@alunos.utfpr.edu.br>
2024-01-21 10:42:48 -0400

    função reiniciar jogo

--- app.js
+++ app.js
@@ -6,17 +6,24 @@ function exibirTextoNaTela(tag, texto) {
     campo.innerHTML = texto;
 }
 
-exibirTextoNaTela('h1', 'Jogo do número secreto');
-exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+function exibirMensagemInicial()
+{
+    exibirTextoNaTela('h1', 'Jogo do número secreto');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+}
+
+exibirMensagemInicial()
 
 function verificarChute() {
     let chute = document.querySelector('input').value;
     let palavrasTentativa = tentativa > 1? 'tentativas' : 'tentativa';
-    
+
     if(chute == numeroSecreto) {
         exibirTextoNaTela('h1','acertou!');
         let mensagemTentativas = `você acertou o número secreto com ${tentativa} ${palavrasTentativa}`;
-        exibirTextoNaTela('p',mensagemTentativas)
+        exibirTextoNaTela('p',mensagemTentativas);
+
+        document.getElementById('reiniciar').removeAttribute('disabled');
    
     } else{
         if(chute > numeroSecreto){
@@ -25,6 +32,7 @@ function verificarChute() {
             exibirTextoNaTela('p','o número secreto é maior');
         }
         tentativa++;
+        limparCampo();
     }
 }
 
@@ -32,7 +40,23 @@ function gerarNumeroAleatorio() {
     return parseInt(Math.random() * 10 + 1);
 }
 
+function limparCampo()
+{
+    chute = document.querySelector('input');
+    chute.value = '';
+}
 
+function reiniciarJogo()
+{
+    numeroSecreto = gerarNumeroAleatorio();
+    limparCampo();
+    tentativa = 1;
+
+    exibirMensagemInicial();
+    document.getElementById('reiniciar').setAttribute('disabled', true);
+
+
+}
 
 
 
