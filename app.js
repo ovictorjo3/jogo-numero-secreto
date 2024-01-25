a625cb886437b59521a990eddb43478334b55e1f
Joao Victor <jsousa.1998@alunos.utfpr.edu.br>
2024-01-25 19:34:04 -0400

    deixando o jogo mais fácil

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaNumerosSorteados = [];
-let numeroLimite = 100;
+let numeroLimite = 50;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativa = 1;
 
@@ -13,7 +13,7 @@ function exibirTextoNaTela(tag, texto) {
 function exibirMensagemInicial()
 {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 50');
 }
 
 exibirMensagemInicial()
